0905ab85c81c59ece290e3391a0a34a4504784b7
trition1846 <bo@boit.dk>
2025-07-04 08:17:19 +0200

    Refactored BooleanCard and enhanced Users functionality
    Modified the `MudCardActions` in `BooleanCard.razor` to simplify its structure and added a `MudSwitch` for improved functionality. Adjusted the `Placeholder` attribute in `Users.razor` and removed the `OnTeamsSelected` method. Added new unit tests in `UsersTests.cs` to verify user deletion, saving, and filtering functionality.

--- Tests/Components/UsersTests.cs
+++ Tests/Components/UsersTests.cs
@@ -50,7 +50,7 @@ public class UsersTests : BaseTestContext
         DialogServiceMock.Verify(m => m.ShowAsync<UserEditorDialog>(
             It.IsAny<string?>(),
             It.Is<DialogParameters>(p => p.Get<User>(nameof(UserEditorDialog.User)) != null && p.Get<User>(nameof(UserEditorDialog.User))!.Id == originalUser.Id),
-            It.IsAny<DialogOptions>()), 
+            It.IsAny<DialogOptions>()),
             Times.Once);
 
         DialogReferenceMock.Verify(m => m.Result, Times.Once);
@@ -61,5 +61,85 @@ public class UsersTests : BaseTestContext
         Assert.Equal(originalUser.FullName, table.Instance.Items.Single(i => i.Id == changedUser.Id).FullName);
     }
 
-    // TODO: Create test to test deletion and updating of users
+    [Fact]
+    public async Task UsersTable_DeletesUser_WhenDeleteButtonClicked()
+    {
+        // Arrange
+        DialogReferenceMock.Setup(m => m.Result).ReturnsAsync(DialogResult.Ok(true));
+        await UserPreferences.SetAsync(nameof(UserPreferences.MimerEnvironment), MimerEnvironment.QA);
+        var cut = RenderComponent<Users>();
+        var tables = cut.FindComponents<MudTable<User>>();
+        var table = Assert.Single(tables);
+        var userToDelete = DefaultData.UsersQA.First();
+
+        // Act
+        await cut.InvokeAsync(() =>
+        {
+            table.Instance.OnRowClick.InvokeAsync(new TableRowClickEventArgs<User>(null!, null!, userToDelete));
+        });
+
+        // Assert
+        // TODO: CosmosServiceMock.Verify(m => m.DeleteUserAsync(userToDelete.Id, MimerEnvironment.QA), Times.Once);
+        Assert.NotNull(table.Instance.Items);
+        Assert.NotEmpty(table.Instance.Items);
+        Assert.DoesNotContain(userToDelete, table.Instance.Items);
+    }
+
+    [Fact]
+    public async Task UsersTable_SavesUser_WhenSaveButtonClicked()
+    {
+        // Arrange
+        await UserPreferences.SetAsync(nameof(UserPreferences.MimerEnvironment), MimerEnvironment.QA);
+        var cut = RenderComponent<Users>();
+        var tables = cut.FindComponents<MudTable<User>>();
+        var table = Assert.Single(tables);
+        var userToSave = DefaultData.UsersQA.First();
+        userToSave.FullName = $"Updated User Name {Guid.NewGuid()}"; // Simulate a change to the user
+        DialogReferenceMock.Setup(m => m.Result).ReturnsAsync(DialogResult.Ok(userToSave));
+
+        // Act
+        await cut.InvokeAsync(() =>
+        {
+            table.Instance.OnRowClick.InvokeAsync(new TableRowClickEventArgs<User>(null!, null!, userToSave));
+        });
+
+        // Assert
+        // TODO: CosmosServiceMock.Verify(m => m.SaveUserAsync(userToSave, MimerEnvironment.QA), Times.Once);
+        Assert.NotNull(table.Instance.Items);
+        Assert.NotEmpty(table.Instance.Items);
+        var updatedUser = table.Instance.Items.Single(i => i.Id == userToSave.Id);
+        Assert.Equal(userToSave.FullName, updatedUser.FullName);
+    }
+
+    [Fact]
+    public async Task UsersTable_FilterCorrectly_WhenFilterApplied()
+    {
+        // Arrange
+        await UserPreferences.SetAsync(nameof(UserPreferences.MimerEnvironment), MimerEnvironment.QA);
+        var users = Fixture.CreateMany<User>(10).ToList();
+        var searchText = Guid.NewGuid().ToString();
+        users.ElementAt(0).Department = $"{users.ElementAt(0).Department}_{searchText}";
+        users.ElementAt(1).Email = $"{users.ElementAt(1).Email}_{searchText}";
+        users.ElementAt(2).FullName = $"{users.ElementAt(2).FullName}_{searchText}";
+        users.ElementAt(3).Location = $"{users.ElementAt(3).Location}_{searchText}";
+        users.ElementAt(4).Id = searchText;
+        CosmosServiceMock.Setup(m => m.GetUsersAsync(MimerEnvironment.QA, It.IsAny<Func<IQueryable<User>, IQueryable<User>>?>()))
+            .ReturnsAsync(users);
+
+        var cut = RenderComponent<Users>();
+        var tables = cut.FindComponents<MudTable<User>>();
+        var table = Assert.Single(tables);
+
+        // Act
+        var mudTextField = cut.FindComponent<MudTextField<string>>();
+        // Set the search text to filter the users
+        await cut.InvokeAsync(() =>
+        {
+            mudTextField.Instance.SetText(searchText);
+        });
+
+        // Assert
+        Assert.NotNull(table.Instance.Items);
+        Assert.True(table.Instance.FilteredItems.OrderBy(i => i.Id).SequenceEqual(users.Take(5).OrderBy(i => i.Id)));
+    }
 }
