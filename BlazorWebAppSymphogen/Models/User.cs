a4f866447081fa2e2ab5cef8334ead92a3926855
trition1846 <bo@boit.dk>
2025-07-08 08:10:08 +0200

    Enhanced user validation and data handling
    Updated `UserEditorDialog.razor` to log and manage unknown and duplicate teams through a new `RemoveInvalidData` method. Improved `Users.razor` by adding a validation status column and refactoring loading logic into `LoadDataAsync`. Modified `User.cs` to implement FluentValidation, adjust the `Teams` property for better data handling, and include a new `ValidationResult` property.

--- BlazorWebAppSymphogen/Models/User.cs
+++ BlazorWebAppSymphogen/Models/User.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using FluentValidation.Results;
+using Newtonsoft.Json;
 
 namespace BlazorWebAppSymphogen.Models;
 
@@ -24,15 +25,29 @@ public class User
     [JsonProperty(PropertyName = "officephone")]
     public string? OfficePhoneNumber { get; set; }
 
+    [JsonIgnore]
+    public string? GroupKey => null;
+
     public IEnumerable<string> TeamIds { get; set; } = [];
 
-    public List<Team> Teams { get; set; } = [];
+    private List<Team> _teams = [];
+    public List<Team> Teams
+    {
+        get
+        {
+            return _teams;
+        }
+        set
+        {
+            _teams = value ?? [];
+            TeamIds = _teams.Select(t => t.Id);
+        }
+    }
 
     [JsonIgnore]// This property is used for validation purposes only and should not be serialized
     public Team? ValidationTeams { get; set; }
 
     public bool UserExists { get; set; } = true;
 
-    [JsonIgnore]
-    public string? GroupKey => null;
+    public ValidationResult? ValidationResult { get; set; }
 }
